328c781a2ba4fceb4b23c770fc67cf3e7272199a
Luciano Decio Pacheco de Almeida Prado Neto <lucianodeciopacheco@gmail.com>
2025-02-02 14:46:13 -0300

    Deploy Vercel

--- my-sales-app/src/environments/environment.ts
+++ my-sales-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 // Variáveis de ambiente para modo em produção
 export const environment = {
-  api: 'https://simulador-vendas-app-angular-18.vercel.app/api/'
+  api: 'https://simulador-vendas-app-angular-19.vercel.app/api/'
 };
