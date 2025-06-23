63093aff885e479b1ebd037cf62ef4602c752e92
Luciano Decio Pacheco de Almeida Prado Neto <lucianodeciopacheco@gmail.com>
2025-06-23 13:17:45 -0300

    Ajustes

--- my-sales-app/src/app/produtos/produtos.component.ts
+++ my-sales-app/src/app/produtos/produtos.component.ts
@@ -12,68 +12,68 @@ import {lastValueFrom} from "rxjs";
 import {NgClass} from "@angular/common";
 
 @Component({
-    selector: 'app-produtos',
-    templateUrl: './produtos.component.html',
-    styleUrl: './produtos.component.css',
-    imports: [MaterialModule, LoadingBarComponent, NgClass]
+  selector: 'app-produtos',
+  templateUrl: './produtos.component.html',
+  styleUrl: './produtos.component.css',
+  imports: [MaterialModule, LoadingBarComponent, NgClass]
 })
 export class ProdutosComponent implements AfterViewInit {
-    @ViewChild(MatPaginator) paginator!: MatPaginator;
-    @ViewChild(MatSort) sort!: MatSort;
-    @ViewChild(MatTable) table!: MatTable<ProdutosItem>;
-    dataSource = new MatTableDataSource<Produto>();
-    showLoading: Boolean = false;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatTable) table!: MatTable<ProdutosItem>;
+  dataSource = new MatTableDataSource<Produto>();
+  showLoading: Boolean = false;
 
 
-    constructor(private produtoService: ProdutosService, private router: Router) {
+  constructor(private produtoService: ProdutosService, private router: Router) {
+  }
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'descricao', 'unidadeMedida', 'ativo', 'editar', 'excluir'];
+
+  ngAfterViewInit(): void {
+    this.loadProdutos();
+  }
+
+  async loadProdutos(): Promise<void> {
+    //Chamada assincrona, para alimentar o data source ele aguarda a resposta do service com os Produtos:
+    this.showLoading = true;
+    let produtos = await lastValueFrom(this.produtoService.getProdutos());
+    this.dataSource = new MatTableDataSource(produtos);
+    this.table.dataSource = this.dataSource;
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+    this.showLoading = false;
+  }
+
+  navegarParaCadProduto() {
+    this.router.navigate(['/cad-produto']);
+  }
+
+  onClickEditar(produto: Produto) {
+    if (produto !== undefined
+      && produto !== null
+      && produto.id !== undefined
+      && produto.id !== null) {
+      this.router.navigate(['/cad-produto-edit', produto.id]);
     }
+  }
 
-    /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-    displayedColumns = ['id', 'descricao', 'unidadeMedida', 'ativo', 'editar', 'excluir'];
-
-    ngAfterViewInit(): void {
-        this.loadProdutos();
-    }
-
-    async loadProdutos(): Promise<void> {
-        //Chamada assincrona, para alimentar o data source ele aguarda a resposta do service com os Produtos:
+  async onClickDeletar(produto: Produto) {
+    if (produto !== undefined
+      && produto !== null
+      && produto.id !== undefined
+      && produto.id !== null) {
+      if (confirm(`Deletar Produto (${produto.id}) ${produto.descricao} ?`)) {
         this.showLoading = true;
-        let produtos = await lastValueFrom(this.produtoService.getProdutos());
-        this.dataSource = new MatTableDataSource(produtos);
-        this.table.dataSource = this.dataSource;
-        this.dataSource.sort = this.sort;
-        this.dataSource.paginator = this.paginator;
-        this.showLoading = false;
+        this.produtoService.deletar(produto.id).subscribe(
+          data => {
+            this.showLoading = false;
+            //Carregar Produtos após deletar:
+            this.loadProdutos();
+          }
+        );
+      }
     }
-
-    navegarParaCadFornecedor() {
-        this.router.navigate(['/cad-produto']);
-    }
-
-    onClickEditar(produto: Produto) {
-        if (produto !== undefined
-            && produto !== null
-            && produto.id !== undefined
-            && produto.id !== null) {
-            this.router.navigate(['/cad-produto-edit', produto.id]);
-        }
-    }
-
-    async onClickDeletar(produto: Produto) {
-        if (produto !== undefined
-            && produto !== null
-            && produto.id !== undefined
-            && produto.id !== null) {
-            if (confirm(`Deletar Produto (${produto.id}) ${produto.descricao} ?`)) {
-                this.showLoading = true;
-                this.produtoService.deletar(produto.id).subscribe(
-                    data => {
-                        this.showLoading = false;
-                        //Carregar Produtos após deletar:
-                        this.loadProdutos();
-                    }
-                );
-            }
-        }
-    }
+  }
 }
